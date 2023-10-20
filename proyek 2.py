8f0786a663718b4b4066e52c1e2d7fcb1a260554
seprianiindi <145238845+seprianiindi@users.noreply.github.com>
2023-10-20 10:55:56 +0800

    Add files via upload

---
+++ proyek 2.py
@@ -0,0 +1,6 @@
+nama=input("siapa nama anda?")
+print("hallo",nama,"semangat belajar ya!!")
+alamat=input("anda dari mana?")
+print("ooh anda dari",alamat,"jauh sekali yaaa")
+nomertelepon=input("berapa nomer telepon anda?")
+print("ooh",nomertelepon,"ini nomer telepon anda baiklah terima kasih!!")
