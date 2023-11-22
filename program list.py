c08a6f242a3156fa6a0e661815f9feb91357f4a2
seprianiindi <145238845+seprianiindi@users.noreply.github.com>
2023-11-22 20:59:42 +0800

    Add files via upload

---
+++ program list.py
@@ -0,0 +1,15 @@
+mahasiswa1 = "yudish"
+print = (mahasiswa1)
+mahasiswa2 = ["indi","anggun","kartika","eka","silpie","istiana","lidia"]
+print(mahasiswa2)
+usia = [17,20,25,29,33] # memiliki urutan di program disebut dengan index
+print(usia)
+jumlahmahasiswa = len(mahasiswa2)
+print(jumlah_mahasiswa)
+status = [True,False,False,True,False]
+print(status)
+print(mahasiswa2[2])
+print(usia[4])
+print(usia[-1])
+mahasiswa2[len(mahasiswa2)-1]
+
