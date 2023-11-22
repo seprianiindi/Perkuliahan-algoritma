9b83997a4c1687b7f8cb8c6c530d6a6d96f48954
seprianiindi <145238845+seprianiindi@users.noreply.github.com>
2023-11-22 20:55:53 +0800

    Add files via upload

---
+++ def menu.py
@@ -0,0 +1,20 @@
+def menu():
+    print("menu ATM uniqbha")
+    print("Hallo mahasiswa")
+    print("1. penarikan")
+    print("2. nabung") 
+    print("3. cek saldo")
+
+
+def hitung_luas_lingkarang(jari):
+    luas = 3.15 *jari*jari
+    print(luas)
+
+
+def hitung_luas_persegi_panjang(panjang,lebar):
+    luas = panjang*lebar
+    print(luas)
+    
+
+ hitung_luas_lingkaran(10)
+ hitung_luas_persegi_panjang(15)
